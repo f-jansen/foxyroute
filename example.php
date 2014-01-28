99132eb9b756de3d28e6d1fa5ead58f7b6b60f73
Fab <mail@fjansen.co.uk>
2014-01-28 14:29:37 +0000

    Tidying up, commenting, some bug fixes!

--- example.php
+++ example.php
@@ -1,20 +1,35 @@
 <?php
 
+/* Simple PHP Router with basic variables support.
+   =============================================== 
+        http://github.com/f-jansen/FoxyRoute       */
+
+//Include
 require_once 'foxyroute.php';
 
+//Initialise
 $foxy = new FoxyRoute();
 
+//Default Route (example.com)
+$foxy->Route("/", function(){
+	
+	print("No route!");
+});
 
-$foxy->Route("/hello", function($foxy){
+//Basic Route (example.com/hello)
+$foxy->Route("/hello", function(){
 	
 	print("Hello!");
-	
 });
 
-$foxy->Route("/hello/{name}", function($foxy){
+//Route with Variable (example.com/hello/tom)
+$foxy->Route("/hello/{name}", function() use ($foxy){
 	
 	print("Hello, " . $foxy->Get('name') . '!');
-	
 });
+
+/* NOTE:
+The first matched route is executed, therefore '/{variable}' will match before {/test}
+This means explicitly named routes should come before similar routes that accept a variable. */
 
 ?>
--- foxyroute.php
+++ foxyroute.php
@@ -1,18 +1,27 @@
 <?php
 
+/* Simple PHP Router with basic variables support.
+   =============================================== 
+        http://github.com/f-jansen/FoxyRoute       */
+
 class FoxyRoute {
 	
 	private $path;
 	private $vars;
+	private $executed;
 	
 	public function __construct(){
 		
-		$path = explode('/', $_SERVER['PATH_INFO']);
-		$this->path = array_filter($path);
+		$this->executed = false;
+		if (isset($_SERVER['PATH_INFO'])){
+			$path = explode('/', $_SERVER['PATH_INFO']);
+			$this->path = array_filter($path);
+		}
 	}
 	
 	public function Debug(){
-		print_r($this->vars);
+		print_r('Path: ' . $this->path . '<br>');
+		print_r('Vars: ' . $this->vars . '<br>');
 	}
 	
 	public function Get($var){
@@ -21,51 +30,51 @@ class FoxyRoute {
 	
 	public function Route($pattern, $function){
 		
-		$pattern = array_filter(explode('/', $pattern));
+		if (!$this->executed){
 		
-		if (count($pattern) == count($this->path)){
+			$pattern = array_filter(explode('/', $pattern));
 			
-			$i = 1;
-			$matches = 0;
-			foreach ($pattern as $p){
-			
-				$p2 = $this->path[$i];
-			
-				if ($this->startsWith($p, '{') && $this->endsWith($p, '}')){
-					
-					$p = str_replace('{', '', str_replace('}', '', $p));
-					
-					$this->vars[$p] = $this->path[$i];
-					
-					$matches++;
-					
-				} else {
-			
-					if ($p == $this->path[$i]){
-						$matches++;
-					} else {
-						break;
-					}
+			if (count($pattern) == 0 && count($pattern) == count($this->path)){
+				$function($this);
+			} else if (count($pattern) == count($this->path)){
 				
+				$i = 1;
+				$matches = 0;
+				foreach ($pattern as $p){
+				
+					$p2 = $this->path[$i];
+				
+					if ($this->startsWith($p, '{') && $this->endsWith($p, '}')){
+					
+						$this->vars[str_replace('{', '', str_replace('}', '', $p))] = $this->path[$i];
+						$matches++;
+						
+					} else {
+				
+						if ($p == $this->path[$i]){
+							$matches++;
+						} else {
+							break;
+						}
+					}
+					$i++;
 				}
 				
-				$i++;
-			}
-			
-			if($matches == count($this->path)){
-				$function($this);
+				if($matches == count($this->path)){
+					$this->executed = true;
+					$function();
+				}
 			}
 		}
 	}
 	
-	function startsWith($haystack, $needle)	{
+	function startsWith($haystack, $needle){
 		return $needle === "" || strpos($haystack, $needle) === 0;
-		}
-	function endsWith($haystack, $needle)	{
+	}
+	
+	function endsWith($haystack, $needle){
 		return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
 	}
 }
-
-
 
 ?>
